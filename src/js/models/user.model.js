2c280e136a42c609810e27d4490cdc671c1574f1
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2015-09-20 17:32:38 +0200

    Start page deisng. Added bubble images for left and right. Initial of register page design.

--- src/js/models/user.model.js
+++ src/js/models/user.model.js
@@ -1,23 +1,19 @@
-(function (window) {
+function User (user) {
+  this.user = user || {};
+  this.user.time = 0;
+  this.user.position = 0;
+}
 
-  function User (user) {
-    this.user = user || {};
-    this.user.time = 0;
-    this.user.position = 0;
-  }
+User.prototype.get = function get () {
+  return user;
+};
 
-  User.prototype.get = function () {
-    return user;
-  };
+User.prototype.setTime = function setTime (time) {
+  this.user.time = time;
+};
 
-  User.prototype.setTime = function (time) {
-    this.user.time = time;
-  };
+User.prototype.setPosition = function setPosition (position) {
+  this.user.position = position;
+};
 
-  User.prototype.setPosition = function (position) {
-    this.user.position = position;
-  };
-
-  window.User = User;
-
-})(window);
+module.exports = User;
