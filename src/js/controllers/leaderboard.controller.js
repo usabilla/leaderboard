f85f3a6684cd4e872bd68cd830fabbe938ee4f51
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2015-09-21 22:52:48 +0200

    Leaderboard load more show less.

--- src/js/controllers/leaderboard.controller.js
+++ src/js/controllers/leaderboard.controller.js
@@ -2,11 +2,25 @@
 function LeaderBoard (GameService) {
   var leaderboard = this;
 
-  leaderboard.users = GameService.getUsers();
+  var users = GameService.getUsers();
+
+  leaderboard.show = true;
+  leaderboard.users = users.slice(0, 8);
 
   leaderboard.areUsers = function areUsers () {
     return leaderboard.users.length > 0;
   };
+
+  leaderboard.load = function load () {
+    var restUsers = users.slice(8);
+    leaderboard.users = leaderboard.users.concat(restUsers);
+    leaderboard.show = false;
+  }
+
+  leaderboard.less = function less () {
+    leaderboard.users = users.slice(0, 8);
+    leaderboard.show = true;
+  }
 }
 
 module.exports = LeaderBoard;
