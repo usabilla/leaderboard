c29017c2bda5d049e8eec44dd32aeb199393f853
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2015-09-23 19:28:28 +0200

    Cancel state change to leaderboard when play again is selected.

--- src/js/controllers/result.controller.js
+++ src/js/controllers/result.controller.js
@@ -5,9 +5,18 @@ function ResultController ($scope, $state, $timeout, GameService) {
   result.user = GameService.getCurrentUser();
   result.position = GameService.getUserPosition(result.user);
 
-  $timeout(function () {
+  result.replay = function replay () {
+    $state.go('count');
+    $timeout.cancel(toLeaderboard);
+  }
+
+  var toLeaderboard = $timeout(function () {
     $state.go('leaderboard');
   }, 5000);
+
+  $scope.$on('$destroy', function () {
+    $timeout.cancel(toLeaderboard);
+  })
 }
 
 module.exports = ResultController;
