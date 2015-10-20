d4ee2d68020ba3758e6938a30c7ec83dd7459c75
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2015-10-20 22:08:02 +0200

    Use gulp load plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,10 @@
 var gulp = require('gulp');
-var angularTemplateCache = require('gulp-angular-templatecache');
-var prefix = require('gulp-autoprefixer');
-var concat = require('gulp-concat');
-var imagemin = require('gulp-imagemin');
-var ngAnnotate = require('gulp-ng-annotate');
-var sass = require('gulp-ruby-sass');
-var uglify = require('gulp-uglify');
+var $ = require('gulp-load-plugins')();
 var addStream = require('add-stream');
 var browserify = require('browserify');
 var del = require('del');
 var buffer = require('vinyl-buffer');
 var source = require('vinyl-source-stream');
-var webserver = require('gulp-webserver');
 
 var paths = {
   dist: 'dist',
@@ -39,7 +32,7 @@ var paths = {
 
 function templates () {
   return gulp.src(paths.partials)
-    .pipe(angularTemplateCache());
+    .pipe($.angularTemplatecache());
 }
 
 gulp.task('clean', function () {
@@ -51,29 +44,29 @@ gulp.task('scripts', function () {
     .bundle()
     .pipe(source('bundle.js'))
     .pipe(buffer())
-    .pipe(ngAnnotate())
+    .pipe($.ngAnnotate())
     .pipe(addStream.obj(templates()))
-    .pipe(concat('app.min.js'))
+    .pipe($.concat('app.min.js'))
     .pipe(gulp.dest(paths.dist + '/js'));
 });
 
 gulp.task('vendor', function () {
   return gulp.src(paths.vendor)
-      .pipe(concat('vendor.min.js'))
+      .pipe($.concat('vendor.min.js'))
     .pipe(gulp.dest(paths.dist + '/js'));
 });
 
 gulp.task('images', function () {
   return gulp.src(paths.images)
-    .pipe(imagemin({optimizationLevel: 5}))
+    .pipe($.imagemin({optimizationLevel: 5}))
     .pipe(gulp.dest(paths.dist + '/images'));
 });
 
 gulp.task('sass', function () {
-  return sass(paths.styles, {style: 'compact'})
-    .pipe(prefix('last 1 version', '> 1%', 'ie 8', 'ie 7'))
-    .pipe(concat('style.css'))
-    .on('error', sass.logError)
+  return $.rubySass(paths.styles, {style: 'compact'})
+    .pipe($.autoprefixer('last 1 version', '> 1%', 'ie 8', 'ie 7'))
+    .pipe($.concat('style.css'))
+    .on('error', $.rubySass.logError)
     .pipe(gulp.dest(paths.dist + '/styles'));
 });
 
@@ -83,18 +76,18 @@ gulp.task('index', function () {
 });
 
 gulp.task('fonts', function () { 
-    return gulp.src(paths.fonts) 
-      .pipe(gulp.dest(paths.dist + '/fonts')); 
+  return gulp.src(paths.fonts) 
+    .pipe(gulp.dest(paths.dist + '/fonts')); 
 });
 
 gulp.task('sounds', function () {
   return gulp.src(paths.sounds) 
-      .pipe(gulp.dest(paths.dist + '/sounds')); 
+    .pipe(gulp.dest(paths.dist + '/sounds')); 
 });
 
-gulp.task('webserver', function () {
+gulp.task('webserver', ['images', 'sounds'], function () {
   gulp.src('dist')
-    .pipe(webserver({
+    .pipe($.webserver({
       livereload: true,
       open: true
     }));
@@ -103,11 +96,30 @@ gulp.task('webserver', function () {
 gulp.task('watch', function () {
   gulp.watch(paths.scripts, ['scripts']);
   gulp.watch(paths.vendor, ['vendor']);
-  gulp.watch([paths.partials], ['scripts']);
+  gulp.watch(paths.partials, ['scripts']);
   gulp.watch(paths.styles, ['sass']);
   gulp.watch(paths.images, ['images']);
 });
 
-gulp.task('build', ['clean', 'scripts', 'vendor', 'sass', 'images', 'fonts', 'sounds', 'index']);
+gulp.task('build', [
+  'clean',
+  'scripts',
+  'vendor',
+  'sass',
+  'images',
+  'fonts',
+  'sounds',
+  'index'
+]);
 
-gulp.task('default', ['scripts', 'vendor', 'sass', 'images', 'fonts', 'sounds', 'index', 'watch', 'webserver']);
+gulp.task('default', [
+  'scripts',
+  'vendor',
+  'sass',
+  'images',
+  'fonts',
+  'sounds',
+  'index',
+  'watch',
+  'webserver'
+]);
