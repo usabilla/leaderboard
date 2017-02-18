7e2816b74684f3dfd4edf1c0d2487f4c41ebcdd6
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2017-02-18 16:23:47 +0100

    build(wallaby): setup wallaby tests

---
+++ wallaby.config.js
@@ -0,0 +1,30 @@
+const wallabyWebpack = require('wallaby-webpack');
+const webpackConfig = require('./webpack.config.js');
+
+webpackConfig.entryPatterns = ['src/**/*.spec.js'];
+
+module.exports = function () {
+  return {
+    debug: true,
+    files: [
+      {pattern: 'src/**/*.html', load: false},
+      {pattern: 'src/**/*.scss', load: false},
+      {pattern: 'config/beforeEachTest.js', load: false},
+      {pattern: 'src/**/*.ts', load: false},
+      {pattern: 'src/**/*.spec.ts', ignore: true}
+    ],
+    tests: [
+      {pattern: 'src/**/*.spec.ts', load: false}
+    ],
+    postprocessor: wallabyWebpack(webpackConfig),
+    env: {
+      type: 'browser',
+      runner: require('phantomjs2-ext').path,
+      params: {runner: '--web-security=false'}
+    },
+    testFramework: 'jasmine',
+    bootstrap: function () {
+      window.__moduleBundler.loadTests();
+    }
+  }
+};
